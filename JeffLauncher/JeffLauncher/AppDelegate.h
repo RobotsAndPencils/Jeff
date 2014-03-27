ce46007cb6ca6a7756abf2285968af394cbf0a70
Brandon Evans <brandon@brandonevans.ca>
2014-03-26 23:16:58 -0600

    First pass at launch-at-login

---
+++ JeffLauncher/JeffLauncher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JeffLauncher
+//
+//  Created by Brandon on 2014-03-26.
+//  Copyright (c) 2014 Robots and Pencils. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
