8b10bfb7adffc2bf62a5585b9c51eab701a08b2f
Brandon Evans <brandon@brandonevans.ca>
2014-05-29 22:53:48 -0600

    Bit of cleanup

--- JeffLauncher/JeffLauncher/AppDelegate.h
+++ JeffLauncher/JeffLauncher/AppDelegate.h
@@ -2,14 +2,12 @@
 //  AppDelegate.h
 //  JeffLauncher
 //
-//  Created by Brandon on 2014-03-26.
+//  Created by Brandon Evans on 2014-03-26.
 //  Copyright (c) 2014 Robots and Pencils. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
-@property (assign) IBOutlet NSWindow *window;
+@property (nonatomic, assign) IBOutlet NSWindow *window;
 
 @end
