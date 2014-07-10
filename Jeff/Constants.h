e4e82e16c384b84593a567a2983e6366b60aa160
Brandon Evans <brandon@brandonevans.ca>
2014-07-09 20:22:35 -0600

    Implement global shortcuts for recording with MASShortcut

---
+++ Jeff/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  Jeff
+//
+//  Created by Brandon Evans on 2014-07-09.
+//  Copyright (c) 2014 Brandon Evans. All rights reserved.
+//
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern NSString *const JEFRecordScreenShortcutKey;
+extern NSString *const JEFRecordSelectionShortcutKey;
+
+#endif
