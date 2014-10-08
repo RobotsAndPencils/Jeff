f67d74868c62f75151710bc28d41a93dca155361
Brandon Evans <brandon@brandonevans.ca>
2014-10-08 15:37:11 -0600

    Extract app/popover/status item code from AD to app controller object

---
+++ Jeff/JEFAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  JEFAppDelegate.h
+//  Jeff
+//
+//  Created by Brandon on 2/21/2014.
+//  Copyright (c) 2014 Brandon Evans. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "JEFSelectionView.h"
+
+@class JEFAppController;
+
+@interface JEFAppDelegate : NSObject <NSApplicationDelegate>
+
+@end
