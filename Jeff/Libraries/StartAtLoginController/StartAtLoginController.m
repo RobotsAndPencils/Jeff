b130e673aecfe54b5980d97b45e0d007a292e9f9
Brandon Evans <brandon@brandonevans.ca>
2014-10-14 14:31:33 -0600

    Attempt to fix crash when initializing StartAtLogin controller
    https://rink.hockeyapp.net/manage/apps/61298/app_versions/5/crash_reasons/21045892

--- Jeff/Libraries/StartAtLoginController/StartAtLoginController.m
+++ Jeff/Libraries/StartAtLoginController/StartAtLoginController.m
@@ -1,3 +1,4 @@
+// Copyright (c) 2014 Brandon Evans
 // Copyright (c) 2011 Alex Zielenski
 // Copyright (c) 2012 Travis Tilley
 //
@@ -25,101 +26,107 @@
 
 @implementation StartAtLoginController
 
-@synthesize identifier = _identifier;
+#pragma mark Lifecycle
 
-#if !__has_feature(objc_arc)
-- (void)dealloc {
-    self.identifier = nil;
-    [super dealloc];
-}
-#endif
-
-+ (BOOL)automaticallyNotifiesObserversForKey:(NSString *)theKey {
-    BOOL automatic = NO;
-    
-    if ([theKey isEqualToString:@"startAtLogin"]) {
-        automatic = NO;
-    } else if ([theKey isEqualToString:@"enabled"]) {
-        automatic = NO;
-    } else {
-        automatic=[super automaticallyNotifiesObserversForKey:theKey];
-    }
-    
-    return automatic;
-}
-
--(id)initWithIdentifier:(NSString*)identifier {
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [self init];
-    if(self) {
-        self.identifier = identifier;
+    if (!self) {
+        return nil;
     }
+
     return self;
 }
 
--(void)setIdentifier:(NSString *)identifier {
+- (id)initWithIdentifier:(NSString *)identifier {
+    self = [self init];
+    if (self) {
+        self.identifier = identifier;
+    }
+
+    return self;
+}
+
+#pragma mark NSKeyValueObserving
+
++ (BOOL)automaticallyNotifiesObserversForKey:(NSString *)theKey {
+    BOOL automatic = NO;
+
+    if ([theKey isEqualToString:@"startAtLogin"]) {
+        automatic = NO;
+    }
+    else if ([theKey isEqualToString:@"enabled"]) {
+        automatic = NO;
+    }
+    else {
+        automatic = [super automaticallyNotifiesObserversForKey:theKey];
+    }
+
+    return automatic;
+}
+
+#pragma mark Properties
+
+- (void)setIdentifier:(NSString *)identifier {
     _identifier = identifier;
     [self startAtLogin];
 #if !defined(NDEBUG)
-    NSLog(@"Launcher '%@' %@ configured to start at login", self.identifier, (_enabled ? @"is" : @"is not"));
+    NSLog(@"Launcher '%@' %@ configured to start at login", _identifier, (self.enabled ? @"is" : @"is not"));
 #endif
 }
 
 - (BOOL)startAtLogin {
-    if (!_identifier)
+    if (!_identifier) {
         return NO;
-    
+    }
+
     BOOL isEnabled = NO;
-    
+
     // the easy and sane method (SMJobCopyDictionary) can pose problems when sandboxed. -_-
     CFArrayRef cfJobDicts = SMCopyAllJobDictionaries(kSMDomainUserLaunchd);
-    NSArray* jobDicts = CFBridgingRelease(cfJobDicts);
-    
+    NSArray *jobDicts = CFBridgingRelease(cfJobDicts);
+
     if (jobDicts && [jobDicts count] > 0) {
-        for (NSDictionary* job in jobDicts) {
+        for (NSDictionary *job in jobDicts) {
             if ([_identifier isEqualToString:[job objectForKey:@"Label"]]) {
                 isEnabled = [[job objectForKey:@"OnDemand"] boolValue];
                 break;
             }
         }
     }
-    
+
     if (isEnabled != _enabled) {
         [self willChangeValueForKey:@"enabled"];
         _enabled = isEnabled;
         [self didChangeValueForKey:@"enabled"];
     }
-    
+
     return isEnabled;
 }
 
 - (void)setStartAtLogin:(BOOL)flag {
-    if (!_identifier)
+    if (!_identifier) {
         return;
-    
+    }
+
     [self willChangeValueForKey:@"startAtLogin"];
-    
+
     if (!SMLoginItemSetEnabled((__bridge CFStringRef)_identifier, (flag) ? true : false)) {
         NSLog(@"SMLoginItemSetEnabled failed!");
-        
+
         [self willChangeValueForKey:@"enabled"];
         _enabled = NO;
         [self didChangeValueForKey:@"enabled"];
-    } else {
+    }
+    else {
         [self willChangeValueForKey:@"enabled"];
         _enabled = YES;
         [self didChangeValueForKey:@"enabled"];
     }
-    
+
     [self didChangeValueForKey:@"startAtLogin"];
 }
 
-- (BOOL)enabled
-{
-    return _enabled;
-}
-
-- (void)setEnabled:(BOOL)enabled
-{
+- (void)setEnabled:(BOOL)enabled {
     [self setStartAtLogin:enabled];
 }
 
