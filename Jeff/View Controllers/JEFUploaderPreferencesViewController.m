92ee001d9d9c7e5a879d5d7facb2d5c9f7e78c2b
Brandon Evans <brandon@brandonevans.ca>
2014-10-17 11:00:20 -0600

    Add email and twitter buttons to the preferences view

--- Jeff/View Controllers/JEFUploaderPreferencesViewController.m
+++ Jeff/View Controllers/JEFUploaderPreferencesViewController.m
@@ -24,6 +24,9 @@
 @property (nonatomic, weak) IBOutlet MASShortcutView *recordScreenShortcutView;
 @property (nonatomic, weak) IBOutlet MASShortcutView *recordSelectionShortcutView;
 @property (nonatomic, strong) IBOutlet NSTextView *openSourceCreditTextView;
+@property (nonatomic, strong) IBOutlet NSTextView *creditTextView;
+@property (nonatomic, weak) IBOutlet NSButton *emailButton;
+@property (nonatomic, weak) IBOutlet NSButton *tweetButton;
 
 @end
 
@@ -32,12 +35,17 @@
 - (void)loadView {
     [super loadView];
 
+    // Initialize Dropbox link button
     __weak __typeof(self) weakSelf = self;
     [[DBAccountManager sharedManager] addObserver:self block:^(DBAccount *account) {
         weakSelf.dropboxLinked = account != nil;
         [weakSelf updateLinkButton];
     }];
-    
+
+    self.dropboxLinked = [[DBAccountManager sharedManager] linkedAccount] != nil;
+    [self updateLinkButton];
+
+    // Initialize MASShortcut views
     self.recordScreenShortcutView.associatedUserDefaultsKey = JEFRecordScreenShortcutKey;
     self.recordSelectionShortcutView.associatedUserDefaultsKey = JEFRecordSelectionShortcutKey;
     self.recordScreenShortcutView.appearance = MASShortcutViewAppearanceTexturedRect;
@@ -54,25 +62,33 @@
         [[Mixpanel sharedInstance] track:@"Change Shortcut" properties:@{ @"Name": @"Record Screen", @"Value": keyCodeString }];
     };
 
-    NSString *htmlString = NSLocalizedString(@"PreferencesOpenSourceCreditsHTML", @"An HTML string containing open source credits");
-    NSData *htmlData = [htmlString dataUsingEncoding:NSUTF8StringEncoding];
-    NSMutableAttributedString *attributedCredits = [[NSMutableAttributedString alloc] initWithHTML:htmlData documentAttributes:NULL];
-    NSDictionary *attributes = @{ NSFontAttributeName : [NSFont systemFontOfSize:[NSFont systemFontSize]], NSForegroundColorAttributeName: [NSColor labelColor] };
-    [attributedCredits addAttributes:attributes range:NSMakeRange(0, attributedCredits.string.length - 1)];
-    self.openSourceCreditTextView.linkTextAttributes = @{ NSForegroundColorAttributeName: [NSColor labelColor], NSCursorAttributeName: [NSCursor pointingHandCursor] };
-    self.openSourceCreditTextView.textStorage.attributedString = attributedCredits;
+    // Initialize rich text credit views
+    [self setupTextView:self.openSourceCreditTextView withHTMLStringWithKey:@"PreferencesOpenSourceCreditsHTML"];
+    [self setupTextView:self.creditTextView withHTMLStringWithKey:@"PreferencesCreditsHTML"];
 
-    self.dropboxLinked = [[DBAccountManager sharedManager] linkedAccount] != nil;
-    [self updateLinkButton];
-    
+    // Initialize version label
     NSString *version = [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"];
     NSString *buildNumber = [[NSBundle mainBundle] infoDictionary][@"CFBundleVersion"];
     self.versionLabel.stringValue = [NSString stringWithFormat:@"You've got Jeff version %@ (Build %@)", version, buildNumber];
 }
 
+
+- (void)viewDidAppear {
+    [super viewDidAppear];
+
+    // Update these each time the view appears in case the user has changed the accounts that are set up
+    NSSharingService *emailService = [NSSharingService sharingServiceNamed:NSSharingServiceNameComposeEmail];
+    self.emailButton.enabled = emailService && [emailService canPerformWithItems:nil];
+
+    NSSharingService *twitterService = [NSSharingService sharingServiceNamed:NSSharingServiceNamePostOnTwitter];
+    self.tweetButton.enabled = twitterService && [twitterService canPerformWithItems:nil];
+}
+
 - (void)dealloc {
     [[DBAccountManager sharedManager] removeObserver:self];
 }
+
+#pragma mark Actions
 
 - (IBAction)toggleLinkDropbox:(id)sender {
     if ([[DBAccountManager sharedManager] linkedAccount]) {
@@ -89,6 +105,35 @@
 - (IBAction)toggleLaunchAtLogin:(id)sender {
     NSButton *checkButton = (NSButton *)sender;
     [[Mixpanel sharedInstance] track:@"Toggle Launch At Login" properties:@{ @"State": @(checkButton.state) }];
+}
+
+- (IBAction)sendEmail:(id)sender {
+    NSSharingService *emailService = [NSSharingService sharingServiceNamed:NSSharingServiceNameComposeEmail];
+    if (!emailService || ![emailService canPerformWithItems:nil]) return;
+
+    emailService.recipients = @[ @"jefftheapp@robotsandpencils.com" ];
+    [emailService performWithItems:@[]];
+}
+
+- (IBAction)sendTweet:(id)sender {
+    NSSharingService *twitterService = [NSSharingService sharingServiceNamed:NSSharingServiceNamePostOnTwitter];
+    if (!twitterService || ![twitterService canPerformWithItems:nil]) return;
+
+    twitterService.recipients = @[ @"jefftheapp" ];
+    [twitterService performWithItems:nil];
+}
+
+#pragma mark Private
+
+- (void)setupTextView:(NSTextView *)view withHTMLStringWithKey:(NSString *)key {
+    NSString *htmlString = NSLocalizedString(key, nil);
+    NSData *htmlData = [htmlString dataUsingEncoding:NSUTF8StringEncoding];
+    NSMutableAttributedString *attributedCredits = [[NSMutableAttributedString alloc] initWithHTML:htmlData documentAttributes:NULL];
+    NSDictionary *attributes = @{ NSFontAttributeName : [NSFont systemFontOfSize:[NSFont systemFontSize]], NSForegroundColorAttributeName: [NSColor labelColor] };
+    [attributedCredits addAttributes:attributes range:NSMakeRange(0, attributedCredits.string.length - 1)];
+    view.linkTextAttributes = @{ NSForegroundColorAttributeName: [NSColor labelColor], NSCursorAttributeName: [NSCursor pointingHandCursor] };
+    view.textStorage.attributedString = attributedCredits;
+
 }
 
 - (void)updateLinkButton {
