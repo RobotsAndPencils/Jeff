c7d8837ccb5cec68c87768aef3567dc7c46aa5e4
Brandon Evans <brandon@brandonevans.ca>
2014-10-25 21:57:24 -0600

    Replace stock Twitter share service with one that shares URL instead of data

---
+++ Jeff/Categories/NSSharingService+ActivityType.h
@@ -0,0 +1,13 @@
+//
+//  NSSharingService+ActivityType.h
+//  Jeff
+//
+//  Created by Brandon Evans on 2014-10-25.
+//  Copyright (c) 2014 Brandon Evans. All rights reserved.
+//
+
+@interface NSSharingService (ActivityType)
+
+@property (nonatomic, strong, readonly) NSString *jef_activityType;
+
+@end
