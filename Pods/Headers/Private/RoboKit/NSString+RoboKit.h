0dc91e833092559b896b0a8902adb13717232bd6
Brandon Evans <brandon@brandonevans.ca>
2015-09-24 20:14:43 -0600

    pod update pop

---
+++ Pods/Headers/Private/RoboKit/NSString+RoboKit.h
@@ -0,0 +1,1 @@
+../../../RoboKit/Classes/Extensions/NSString+RoboKit.h
--- Pods/Target Support Files/Pods-JeffTests-MASShortcut/Pods-JeffTests-MASShortcut-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JeffTests_MASShortcut : NSObject
-@end
-@implementation PodsDummy_Pods_JeffTests_MASShortcut
-@end
